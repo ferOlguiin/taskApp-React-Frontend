e48655230759e035f498260b03773bd912797cd3
ferOlguiin <feergui997@hotmail.com>
2023-04-10 20:41:59 -0300

    footerLinks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import { BsGithub, BsLinkedin } from "react-icons/bs"
 
 export const Footer = () => {
+
   return (
     <div className="fixed-bottom d-flex justify-content-end align-items-center py-3 px-5 bg-black text-light">
-        <span><BsGithub className="fs-4 me-2"/></span>
-        <span><BsLinkedin className="text-primary fs-4"/></span>
+        <a className="text-decoration-none" href="https://github.com/ferOlguiin?tab=repositories" rel="noreferrer" target="_blank"><BsGithub className="fs-4 me-2"/></a>
+        <a className="text-decoration-none" href="https://www.linkedin.com/in/fernando-olguin-5a63a9236/" rel="noreferrer" target="_blank"><BsLinkedin className="text-primary fs-4"/></a>
     </div>
   )
 }
