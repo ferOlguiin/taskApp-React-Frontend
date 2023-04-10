f98e08a642016ad7b8f9d0a669abaf06f61c96cc
ferOlguiin <feergui997@hotmail.com>
2023-04-10 20:29:21 -0300

    URL add

--- src/api/backendConnection.js
+++ src/api/backendConnection.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+const url = import.meta.env.VITE_BASE_URL;
 
-export const getUserRequest = async (fields) => await axios.post("http://localhost:3000/user", fields, {
+export const getUserRequest = async (fields) => await axios.post(`${url}/user`, fields, {
     withCredentials: true
 });
 
-export const getTaskRequest = async (email) => await axios.get("http://localhost:3000/allpost/" + email);
-export const createTaskRequest = async (fields) => await axios.post("http://localhost:3000/post", fields);
-export const deleteTaskRequest = async (id) => await axios.delete("http://localhost:3000/post/" + id);
-export const getOneTaskRequest = async (id) => await axios.get("http://localhost:3000/post/" + id);
-export const editTaskRequest = async (fields, id) => await axios.put("http://localhost:3000/post/" + id, fields);
+export const getTaskRequest = async (email) => await axios.get(`${url}/allpost/` + email);
+export const createTaskRequest = async (fields) => await axios.post(`${url}/post`, fields);
+export const deleteTaskRequest = async (id) => await axios.delete(`${url}/post/` + id);
+export const getOneTaskRequest = async (id) => await axios.get(`${url}/post/` + id);
+export const editTaskRequest = async (fields, id) => await axios.put(`${url}/post/` + id, fields);
