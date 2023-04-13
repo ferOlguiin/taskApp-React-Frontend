8b4a754dd6c31b4f75858514f380c82ccf6f6bb3
ferOlguiin <feergui997@hotmail.com>
2023-04-13 14:51:20 -0300

    registerchanges2

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import {VscTools} from "react-icons/vsc";
 
 export const Register = () => {
   return (
-    <div className="d-flex min-vh-100 justify-content-center align-items-center">
+    <div className="d-flex min-vh-100 justify-content-center align-items-center text-white fs-5">
       Aún no implementado... <VscTools/>
     </div>
   )
